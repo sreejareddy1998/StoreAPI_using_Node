83dcdfaba5d741a083eced44876bd99c9cca256c
Sreeja <sreejamadireddy1998@gmail.com>
2023-07-26 16:29:28 +0530

    Adding regex

--- controllers/product.js
+++ controllers/product.js
@@ -1,17 +1,24 @@
 const Product=require('../models/products');
 const getAllProductsStatic= async (req,res)=>{
-    const products=await Product.find({featured:true})
+    const search='a'
+    const products=await Product.find({name:
+        { $regex:search, $options: 'i'}})
+    //option i means case insensitive
     // throw new Error('Testing async errors')
     res.status(200).json({products, no_of_hits: products.length})
 }
 const getAllProducts= async (req,res)=>{
-    const {featured, company}= req.query;
+    const {featured, company, name}= req.query;
     const queryObject={}
     if(featured){
         queryObject.featured= featured=== true? 'true':'false'
     }
     if(company){
         queryObject.company= company
+    }
+    if(name)
+    {
+        queryObject.name={ $regex:name, $options: 'i'}
     }
     const products=await Product.find(queryObject)
     res.status(200).json({products, no_of_hits:products.length})
