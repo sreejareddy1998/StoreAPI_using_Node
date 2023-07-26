06e4df86f33a131c2e55c36dc585ec0417d816c2
Sreeja <sreejamadireddy1998@gmail.com>
2023-07-26 18:33:02 +0530

    Adding skip and limit in getAllProductsStatic

--- controllers/product.js
+++ controllers/product.js
@@ -1,35 +1,38 @@
-const Product=require('../models/products');
-const getAllProductsStatic= async (req,res)=>{
-    const products=await Product.find({}).sort('-name price')
-    //option i means case insensitive
-    // throw new Error('Testing async errors')
-    res.status(200).json({products, no_of_hits: products.length})
-}
-const getAllProducts= async (req,res)=>{
-    const {featured, company, name, sort}= req.query;
-    const queryObject={}
-    if(featured){
-        queryObject.featured= featured=== true? 'true':'false'
-    }
-    if(company){
-        queryObject.company= company
-    }
-    if(name)
-    {
-        queryObject.name={ $regex:name, $options: 'i'}
-    }
-    let result=Product.find(queryObject)
-    if(sort)
-    {
-        const sortList=sort.split(',').join(' ')
-        result=result.sort(sortList)
-    }
-    else
-    {
-        resut=result.sort('createdAt')
-    }
-    const products= await result
-    res.status(200).json({products, no_of_hits:products.length})
-}
+const Product = require("../models/products");
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({}).sort("name").select('name price').limit(10).skip(1)
+  //option i means case insensitive
+  // throw new Error('Testing async errors')
+  res.status(200).json({ products, no_of_hits: products.length });
+};
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort , fields} = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === true ? "true" : "false";
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  let result = Product.find(queryObject);
+  //sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    resut = result.sort("createdAt");
+  }
+  //selecting only particular fields
+  if(fields)
+  {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  const products = await result;
+  res.status(200).json({ products, no_of_hits: products.length });
+};
 
-module.exports = {getAllProductsStatic, getAllProducts}
+module.exports = { getAllProductsStatic, getAllProducts };
