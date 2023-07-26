62012bc6833d4b171321c391c2ef5cc8dbdb061d
Sreeja <sreejamadireddy1998@gmail.com>
2023-07-26 15:33:00 +0530

    adjusting lines

--- populate.js
+++ populate.js
@@ -1,10 +1,7 @@
 require('dotenv').config()
-
 const connectDB= require('./db/connect');
 const Product=require('./models/products');
-
 const jsonProducts=require('./products.json');
-
 const start=async()=>{
     try{
         await connectDB(process.env.MONGO_URI)
