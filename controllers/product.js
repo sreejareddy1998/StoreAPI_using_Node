c9444a88b0f5d488b821640a2c3fe7b024a8b905
Sreeja <sreejamadireddy1998@gmail.com>
2023-07-26 16:03:03 +0530

    updating controllers with find login

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,12 @@
+const Product=require('../models/products');
 const getAllProductsStatic= async (req,res)=>{
-    throw new Error('Testing async errors')
-    res.status(200).json({msg : `Products testing route`})
+    const products=await Product.find({featured:true})
+    // throw new Error('Testing async errors')
+    res.status(200).json({products, no_of_hits: products.length})
 }
 const getAllProducts= async (req,res)=>{
-    res.status(200).json({msg : `Products route`})
+    const products=await Product.find(req.query)
+    res.status(200).json({products, no_of_hits:products.length})
 }
 
 module.exports = {getAllProductsStatic, getAllProducts}
